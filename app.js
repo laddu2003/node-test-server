f6bedbdeb9e3885929cae9e99729989d90c30b1c
Gaurav Palaspagar <68456945+laddu2003@users.noreply.github.com>
2021-06-02 20:33:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const http = require('http')
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
+const express = require('express')
+const port = 5000;
+const app  =  express()
+app.get('/',(req, res) => {
   res.end('Hello World! I am your new NodeJS app! \n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(port, () => {
+  console.log(`Server running at ${port}`);
 });
